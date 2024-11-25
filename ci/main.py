fc84da60fdc0941adf42bd977ad3baedf05c09de
Dharjai15 <dharshiniganesan1515@gmail.com>
2024-11-25 06:20:48 +0530

    Update main.py

--- ci/main.py
+++ ci/main.py
@@ -1,1 +1,25 @@
+def hello():
+    print("hi")
 
+
+def bye():
+    print("bye")
+gitup/workflows/Super-linter.yml
+
+name: Super-Linter
+
+on: push
+
+jobs:
+  super-lint:
+    name: Lint code base
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout code
+        uses: actions/checkout@v2
+
+      - name: Run Super-Linter
+        uses: github/super-linter@v4
+        env:
+          DEFAULT_BRANCH: main
+          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
